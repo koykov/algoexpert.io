fa851a692448af786e6918445c53c0932e272600
Konstantin Voykov <koykov0@gmail.com>
2023-09-30 00:22:47 +0300

    Bubble Sort: final solution; test/bench.

---
+++ bubble-sort/bubble_sort.go
@@ -0,0 +1,12 @@
+package bubble_sort
+
+func BubbleSort(a []int) []int {
+	for i := len(a) - 1; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+	return a
+}
