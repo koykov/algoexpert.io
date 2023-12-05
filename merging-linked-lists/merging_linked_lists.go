30e125cf3b518998fc96fc37186f4be07b6eb588
Konstantin Voykov <koykov0@gmail.com>
2023-12-06 00:16:49 +0200

    Merging Linked Lists: final solution; test.

--- merging-linked-lists/merging_linked_lists.go
+++ merging-linked-lists/merging_linked_lists.go
@@ -6,6 +6,19 @@ type LinkedList struct {
 }
 
 func MergingLinkedLists(a *LinkedList, b *LinkedList) *LinkedList {
+	al, bl := lenOf(a), lenOf(b)
+
+	switch {
+	case al > bl:
+		for i := 0; i < al-bl; i++ {
+			a = a.Next
+		}
+	case bl > al:
+		for i := 0; i < bl-al; i++ {
+			b = b.Next
+		}
+	}
+
 	if a == b && a != nil {
 		return a
 	}
@@ -13,4 +26,11 @@ func MergingLinkedLists(a *LinkedList, b *LinkedList) *LinkedList {
 		return nil
 	}
 	return MergingLinkedLists(a.Next, b.Next)
+}
+
+func lenOf(l *LinkedList) int {
+	if l == nil {
+		return 0
+	}
+	return lenOf(l.Next) + 1
 }
