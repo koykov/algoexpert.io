05dac8f993e55ca63d077656ec3c9d2e0e6b41b8
Konstantin Voykov <koykov0@gmail.com>
2023-12-10 22:30:55 +0200

    Staircase Traversal: final solution; test.

--- staircase-traversal/staircase_traversal.go
+++ staircase-traversal/staircase_traversal.go
@@ -1,10 +1,11 @@
 package staircase_traversal
 
 func StaircaseTraversal(h int, s int) (r int) {
-	if h == 0 || h == 1 {
-		return 1
+	if h == 0 {
+		r = 1
+		return
 	}
-	for i := 1; i <= s; i++ {
+	for i := 1; i <= s && i <= h; i++ {
 		r += StaircaseTraversal(h-i, s)
 	}
 	return
