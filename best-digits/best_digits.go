35f871f4d0cba76983fd5817a0d84859b80295fd
Konstantin Voykov <koykov0@gmail.com>
2023-12-18 18:42:08 +0200

    Best Digits: final solution; test.

--- best-digits/best_digits.go
+++ best-digits/best_digits.go
@@ -3,19 +3,18 @@ package best_digits
 import "strconv"
 
 func BestDigits(n string, d int) string {
+	d1 := d
 	buf := make([]int, 0, len(n))
 	for i := 0; i < len(n); i++ {
 		n1, _ := strconv.Atoi(string(n[i]))
-		if d > 0 && len(buf) > 0 {
-			if n1 >= buf[len(buf)-1] {
-				buf = buf[:len(buf)-1]
-				d--
-			}
+		for d > 0 && len(buf) > 0 && n1 > buf[len(buf)-1] {
+			buf = buf[:len(buf)-1]
+			d--
 		}
 		buf = append(buf, n1)
 	}
 	r := make([]byte, 0, len(buf))
-	for i := 0; i < len(buf); i++ {
+	for i := 0; i < len(n)-d1; i++ {
 		r = strconv.AppendInt(r, int64(buf[i]), 10)
 	}
 	return string(r)
