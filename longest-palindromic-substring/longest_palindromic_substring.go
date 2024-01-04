3b57a619f6d49e8da7d12400aa236d0fe856afb5
Konstantin Voykov <koykov0@gmail.com>
2024-01-04 18:31:21 +0200

    Longest Palindromic Substring: draft solution v2; test.

--- longest-palindromic-substring/longest_palindromic_substring.go
+++ longest-palindromic-substring/longest_palindromic_substring.go
@@ -1,5 +1,37 @@
 package longest_palindromic_substring
 
 func LongestPalindromicSubstring(s string) string {
-	return s
+	n := len(s)
+	if n < 2 {
+		return s
+	}
+	var (
+		mp string
+		ml int
+	)
+	for i := 0; i < n; i++ {
+		l, r := i, i
+		if r < n-1 && s[i] == s[i+1] {
+			r++
+		}
+		for l > 0 && r < n-1 {
+			l--
+			r++
+			if s[l] != s[r] {
+				break
+			}
+		}
+		ds := s[l : r+1]
+		if len(ds) > 1 && ds[0] != ds[len(ds)-1] {
+			ds = ds[1 : len(ds)-1]
+		}
+		if len(ds) > ml {
+			ml = len(ds)
+			mp = s[l : r+1]
+		}
+	}
+	if len(mp) > 1 && mp[0] != mp[len(mp)-1] {
+		mp = mp[1 : len(mp)-1]
+	}
+	return mp
 }
--- longest-palindromic-substring/longest_palindromic_substring_test.go
+++ longest-palindromic-substring/longest_palindromic_substring_test.go
@@ -7,7 +7,24 @@ import (
 )
 
 func TestLongestPalindromicSubstring(t *testing.T) {
-	expected := "xyzzyx"
-	output := LongestPalindromicSubstring("abaxyzzyxf")
-	require.Equal(t, expected, output)
+	t.Run("abaxyzzyxf", func(t *testing.T) {
+		expected := "xyzzyx"
+		output := LongestPalindromicSubstring("abaxyzzyxf")
+		require.Equal(t, expected, output)
+	})
+	t.Run("it's highnoon", func(t *testing.T) {
+		expected := "noon"
+		output := LongestPalindromicSubstring("it's highnoon")
+		require.Equal(t, expected, output)
+	})
+	t.Run("abcdefgfedcbazzzzzzzzzzzzzzzzzzzz", func(t *testing.T) {
+		expected := "zzzzzzzzzzzzzzzzzzzz"
+		output := LongestPalindromicSubstring("abcdefgfedcbazzzzzzzzzzzzzzzzzzzz")
+		require.Equal(t, expected, output)
+	})
+	t.Run("z234a5abbba54a32z", func(t *testing.T) {
+		expected := "5abbba5"
+		output := LongestPalindromicSubstring("z234a5abbba54a32z")
+		require.Equal(t, expected, output)
+	})
 }
