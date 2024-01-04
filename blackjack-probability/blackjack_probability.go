9ad0943544fa2d35cd61b881deeaae71d986e075
Konstantin Voykov <koykov0@gmail.com>
2024-01-04 23:37:56 +0200

    Blackjack Probability: final solution; test.

--- blackjack-probability/blackjack_probability.go
+++ blackjack-probability/blackjack_probability.go
@@ -1,6 +1,14 @@
 package blackjack_probability
 
-func BlackjackProbability(t, s int) float64 {
-	// todo implement me
-	return -1
+func BlackjackProbability(t int, h int) float64 {
+	pb := []float64{0, 0, 0, 0, 0, 1, 1, 1, 1, 1}
+	for v := t - 5; v >= h; v-- {
+		s := float64(0)
+		for _, prob := range pb {
+			s += prob / 10
+		}
+		copy(pb[1:], pb[:len(pb)-1])
+		pb[0] = s
+	}
+	return pb[0]
 }
