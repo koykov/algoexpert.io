ce7567ccb6f1d6b6415a2bbb4ee544349693b2c5
Konstantin Voykov <koykov0@gmail.com>
2024-01-10 22:24:49 +0200

    Minimum Characters For Words: final solution; test.

--- minimum-characters-for-words/minimum_characters_for_words.go
+++ minimum-characters-for-words/minimum_characters_for_words.go
@@ -2,14 +2,22 @@ package minimum_characters_for_words
 
 func MinimumCharactersForWords(words []string) []string {
 	r := make([]string, 0)
-	idx := make(map[byte]int)
+	idx, tidx := make(map[byte]int), make(map[byte]int)
 	for i := 0; i < len(words); i++ {
+		for k := range idx {
+			delete(idx, k)
+		}
 		w := words[i]
 		for j := 0; j < len(w); j++ {
 			idx[w[j]]++
 		}
+		for k, v := range idx {
+			if tidx[k] < v {
+				tidx[k] = v
+			}
+		}
 	}
-	for b, c := range idx {
+	for b, c := range tidx {
 		s := string(b)
 		for i := 0; i < c; i++ {
 			r = append(r, s)
