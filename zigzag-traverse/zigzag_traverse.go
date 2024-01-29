7db1ce671d662e788a1a8a3dc8f9d37466ebe76e
Konstantin Voykov <koykov0@gmail.com>
2024-01-29 22:21:40 +0200

    Zigzag Traverse: final solution; test.

--- zigzag-traverse/zigzag_traverse.go
+++ zigzag-traverse/zigzag_traverse.go
@@ -1,14 +1,36 @@
 package zigzag_traverse
 
-import "fmt"
-
 func ZigzagTraverse(a [][]int) []int {
-	n, m := len(a), len(a[0])
-	r := make([]int, n*m)
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			fmt.Printf("[%d, %d] %d\n", i, j, a[i][j])
+	n1, m := len(a), len(a[0])
+	r := make([]int, 0, n1*m)
+	n := max(n1, m)
+	for i := 0; i < n*2; i++ {
+		if i%2 == 0 {
+			y := i
+			for j := 0; j <= i; j++ {
+				x := j
+				if x < n1 && y < m {
+					r = append(r, a[x][y])
+				}
+				y--
+			}
+		} else {
+			y := 0
+			for j := i; j >= 0; j-- {
+				x := j
+				if x < n1 && y < m {
+					r = append(r, a[x][y])
+				}
+				y++
+			}
 		}
 	}
 	return r
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- zigzag-traverse/zigzag_traverse_test.go
+++ zigzag-traverse/zigzag_traverse_test.go
@@ -7,13 +7,23 @@ import (
 )
 
 func TestZigzagTraverse(t *testing.T) {
-	input := [][]int{
-		{1, 3, 4, 10},
-		{2, 5, 9, 11},
-		{6, 8, 12, 15},
-		{7, 13, 14, 16},
-	}
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	output := ZigzagTraverse(input)
-	require.Equal(t, expected, output)
+	t.Run("0", func(t *testing.T) {
+		input := [][]int{
+			{1, 3, 4, 10},
+			{2, 5, 9, 11},
+			{6, 8, 12, 15},
+			{7, 13, 14, 16},
+		}
+		expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+		output := ZigzagTraverse(input)
+		require.Equal(t, expected, output)
+	})
+	t.Run("1", func(t *testing.T) {
+		input := [][]int{
+			{1, 2, 3, 4, 5},
+		}
+		expected := []int{1, 2, 3, 4, 5}
+		output := ZigzagTraverse(input)
+		require.Equal(t, expected, output)
+	})
 }
