f522d2c2face69011352779cd3377ff0e798c1b2
Konstantin Voykov <koykov0@gmail.com>
2024-01-30 23:26:18 +0200

    Longest Subarray With Sum: draft solution; test.

--- longest-subarray-with-sum/longest_subarray_with_sum.go
+++ longest-subarray-with-sum/longest_subarray_with_sum.go
@@ -1,22 +1,27 @@
 package longest_subarray_with_sum
 
 func LongestSubarrayWithSum(a []int, t int) []int {
-	var lo, hi, rlo, rhi, s int
+	var lo, hi, l, h, s int
 	for i := 0; i < len(a); i++ {
-		switch {
-		case s < t:
+		if s < t {
 			hi = i
 			s += a[i]
+		}
+		switch {
+		case s < t:
+			continue
 		case s == t:
-			if hi-lo > rhi-rlo {
-				rlo, rhi = lo, hi
+			if hi-lo > h-l {
+				l, h = lo, hi
 			}
+			s -= a[lo]
+			lo++
 		case s > t:
 			s -= a[lo]
 			lo++
 			i--
 		}
 	}
-	r := [2]int{rlo, rhi}
+	r := [2]int{l, h}
 	return r[:]
 }
