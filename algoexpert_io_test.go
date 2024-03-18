692bbb9e4ca7f44a038e8838c68814c5d4293c2c
Konstantin Voykov <koykov0@gmail.com>
2024-03-18 23:05:23 +0200

    FS issues: make global go.mod file.

---
+++ algoexpert_io_test.go
@@ -0,0 +1,13 @@
+package algoexpert_io
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAlgoexpertIO(t *testing.T) {
+	expected := 13
+	actual := 13
+	require.Equal(t, expected, actual)
+}
