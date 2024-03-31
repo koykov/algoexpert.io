a25626780b3c7ed8f129c2d1d02a513a5217946b
Konstantin Voykov <koykov0@gmail.com>
2024-03-31 23:20:12 +0300

    Longest Common Subsequence: draft solution; test.

--- longest-common-subsequence/longest_common_subsequence.go
+++ longest-common-subsequence/longest_common_subsequence.go
@@ -1,19 +1,40 @@
 package longest_common_subsequence
 
 func LongestCommonSubsequence(s1 string, s2 string) []byte {
-	mn, mx := s1, s2
-	if len(mn) > len(mx) {
-		mn, mx = mx, mn
+	n, m := len(s1), len(s2)
+	var x [][]int
+	for i := 0; i < m; i++ {
+		x = append(x, make([]int, m))
+		if i > 0 {
+			copy(x[i], x[i-1])
+		}
+		var inc bool
+		for j := 0; j < n; j++ {
+			if s2[i] == s1[j] {
+				inc = true
+			}
+			if inc {
+				x[i][j]++
+			}
+		}
 	}
-	r := make(map[byte]struct{}, len(mn))
-	for i := 0; i < len(mn); i++ {
-		r[mn[i]] = struct{}{}
-	}
-	buf := make([]byte, 0, len(mn))
-	for i := 0; i < len(mx); i++ {
-		if _, ok := r[mx[i]]; ok {
-			buf = append(buf, mx[i])
+
+	var buf []byte
+	var p int
+	for i := 0; i < n; i++ {
+		if x[m-1][i] != p {
+			p = x[m-1][i]
+			buf = append(buf, s1[i])
 		}
 	}
 	return buf
+}
+
+func print_(x [][]int) {
+	for i := 0; i < len(x); i++ {
+		for j := 0; j < len(x[i]); j++ {
+			print(x[i][j], " ")
+		}
+		print("\n")
+	}
 }
