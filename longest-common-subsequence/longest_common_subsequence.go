f9f0e31ce14576fceeb3e5cb07d3ce877556bbeb
Konstantin Voykov <koykov0@gmail.com>
2024-04-08 23:38:39 +0300

    Longest Common Subsequence: final solution; test.

--- longest-common-subsequence/longest_common_subsequence.go
+++ longest-common-subsequence/longest_common_subsequence.go
@@ -1,40 +1,25 @@
 package longest_common_subsequence
 
 func LongestCommonSubsequence(s1 string, s2 string) []byte {
-	n, m := len(s1), len(s2)
-	var x [][]int
-	for i := 0; i < m; i++ {
-		x = append(x, make([]int, m))
-		if i > 0 {
-			copy(x[i], x[i-1])
-		}
-		var inc bool
-		for j := 0; j < n; j++ {
-			if s2[i] == s1[j] {
-				inc = true
-			}
-			if inc {
-				x[i][j]++
-			}
-		}
+	m, n := len(s1), len(s2)
+	var x [][][]byte
+	for i := 0; i < n+1; i++ {
+		x = append(x, make([][]byte, m+1))
 	}
 
-	var buf []byte
-	var p int
-	for i := 0; i < n; i++ {
-		if x[m-1][i] != p {
-			p = x[m-1][i]
-			buf = append(buf, s1[i])
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if s1[j-1] == s2[i-1] {
+				x[i][j] = append(x[i][j], x[i-1][j-1]...)
+				x[i][j] = append(x[i][j], s2[i-1])
+			} else {
+				if len(x[i][j-1]) >= len(x[i-1][j]) {
+					x[i][j] = x[i][j-1]
+				} else {
+					x[i][j] = x[i-1][j]
+				}
+			}
 		}
 	}
-	return buf
-}
-
-func print_(x [][]int) {
-	for i := 0; i < len(x); i++ {
-		for j := 0; j < len(x[i]); j++ {
-			print(x[i][j], " ")
-		}
-		print("\n")
-	}
+	return x[n][m]
 }
--- longest-common-subsequence/longest_common_subsequence_test.go
+++ longest-common-subsequence/longest_common_subsequence_test.go
@@ -7,7 +7,14 @@ import (
 )
 
 func TestLongestCommonSubsequence(t *testing.T) {
-	expected := []byte("XYZW")
-	output := LongestCommonSubsequence("ZXVVYZW", "XKYKZPW")
-	require.Equal(t, expected, output)
+	t.Run("0", func(t *testing.T) {
+		expected := []byte("XYZW")
+		output := LongestCommonSubsequence("ZXVVYZW", "XKYKZPW")
+		require.Equal(t, expected, output)
+	})
+	t.Run("1", func(t *testing.T) {
+		expected := []byte("AE")
+		output := LongestCommonSubsequence("ABCDEFG", "APPLES")
+		require.Equal(t, expected, output)
+	})
 }
