3c5749322ce4a8527b170928f18641da6c52f383
Konstantin Voykov <koykov0@gmail.com>
2024-04-12 21:54:32 +0300

    Min Number Of Jumps: final solution; test.

--- min-number-of-jumps/min_number_of_jumps.go
+++ min-number-of-jumps/min_number_of_jumps.go
@@ -1,8 +1,11 @@
 package min_number_of_jumps
 
 func MinNumberOfJumps(a []int) int {
+	if len(a) == 1 {
+		return 0
+	}
 	reach, s, j := a[0], a[0], 0
-	for i := 1; i < len(a); i++ {
+	for i := 1; i < len(a)-1; i++ {
 		reach = max(reach, a[i]+i)
 		if s = s - 1; s == 0 {
 			j++
