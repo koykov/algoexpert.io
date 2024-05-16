edc56db9c706c4b635a7020a4888a839cdb84534
Konstantin Voykov <koykov0@gmail.com>
2024-05-16 18:14:00 +0300

    Maximize Expression: draft solution; test.

--- maximize-expression/maximize_expression.go
+++ maximize-expression/maximize_expression.go
@@ -1,75 +1,37 @@
 package maximize_expression
 
-import "math"
+const minInt = -1e9
 
 func MaximizeExpression(arr []int) int {
-	n := len(arr)
-	if n < 4 {
+	r := maxp(arr, 0, 2, 0)
+	if r <= minInt {
 		return 0
 	}
-	var mx, mn, a, b, c, d int
-	buf := make([]int, n)
+	return r
+}
 
-	// maximize a
-	mx = -math.MaxInt
-	for i := 0; i < n; i++ {
-		mx = max(arr[i], mx)
-		buf[i] = mx
+func maxp(a []int, i, k int, state int) (r int) {
+	if i >= len(a) {
+		return minInt
 	}
-	mx = -math.MaxInt
-	for i := 0; i < n-3; i++ {
-		if buf[i] > mx {
-			mx = buf[i]
-			a = i
+	if k == 0 {
+		return 0
+	}
+	r = minInt
+	if state == 0 {
+		t := maxp(a, i+1, k, 1)
+		if t != minInt {
+			t = a[i] + maxp(a, i+1, k, 1)
 		}
-	}
-	clear(buf)
-
-	// minimize b
-	mn = math.MaxInt
-	for i := a + 1; i < n; i++ {
-		mn = min(mn, arr[i])
-		buf[i] = mn
-	}
-	mn = math.MaxInt
-	for i := a + 1; i < n-2; i++ {
-		if buf[i] < mn {
-			mn = buf[i]
-			b = i
+		r = max(r, max(maxp(a, i+1, k, 0), t))
+	} else {
+		t := maxp(a, i+1, k-1, 0)
+		if t != minInt {
+			t = -a[i] + maxp(a, i+1, k-1, 0)
 		}
+		r = max(r, max(maxp(a, i+1, k, 1), t))
 	}
-	clear(buf)
-
-	// maximize c
-	mx = -math.MaxInt
-	for i := c + 1; i < n; i++ {
-		mx = max(arr[i], mx)
-		buf[i] = mx
-	}
-	mx = -math.MaxInt
-	for i := b + 1; i < n-1; i++ {
-		if buf[i] > mx {
-			mx = buf[i]
-			c = i
-		}
-	}
-	clear(buf)
-
-	// minimize d
-	mn = math.MaxInt
-	for i := a + 1; i < n; i++ {
-		mn = min(mn, arr[i])
-		buf[i] = mn
-	}
-	mn = math.MaxInt
-	for i := c + 1; i < n; i++ {
-		if buf[i] < mn {
-			mn = buf[i]
-			d = i
-		}
-	}
-
-	return arr[a] - arr[b] + arr[c] - arr[d]
+	return
 }
 
 func max(a, b int) int {
@@ -77,17 +39,4 @@ func max(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func clear(arr []int) {
-	for i := 0; i < len(arr); i++ {
-		arr[i] = 0
-	}
 }
--- maximize-expression/maximize_expression_test.go
+++ maximize-expression/maximize_expression_test.go
@@ -7,8 +7,16 @@ import (
 )
 
 func TestMaximizeExpression(t *testing.T) {
-	input := []int{3, 6, 1, -3, 2, 7}
-	expected := 4
-	actual := MaximizeExpression(input)
-	require.Equal(t, expected, actual)
+	t.Run("0", func(t *testing.T) {
+		input := []int{3, 6, 1, -3, 2, 7}
+		expected := 4
+		actual := MaximizeExpression(input)
+		require.Equal(t, expected, actual)
+	})
+	t.Run("1", func(t *testing.T) {
+		input := []int{3, 9, 10, 1, 30, 40}
+		expected := 3
+		actual := MaximizeExpression(input)
+		require.Equal(t, expected, actual)
+	})
 }
