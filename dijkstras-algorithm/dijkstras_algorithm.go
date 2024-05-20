124356a8d90b4b5c622b856a46944a3284498dd1
Konstantin Voykov <koykov0@gmail.com>
2024-05-20 23:31:30 +0300

    Dijkstra's Algorithm: final solution; test.

--- dijkstras-algorithm/dijkstras_algorithm.go
+++ dijkstras-algorithm/dijkstras_algorithm.go
@@ -4,11 +4,13 @@ import "math"
 
 func DijkstrasAlgorithm(start int, edges [][][]int) []int {
 	buf := make([]int, len(edges))
+	v := make([]bool, len(edges))
 	for i := 0; i < len(edges); i++ {
 		buf[i] = math.MaxInt
 	}
 	buf[start] = 0
-	buf = bfs(buf, start, 0, edges)
+	v[start] = true
+	buf = bfs(buf, start, 0, edges, v)
 	for i := 0; i < len(buf); i++ {
 		if buf[i] == math.MaxInt {
 			buf[i] = -1
@@ -17,15 +19,19 @@ func DijkstrasAlgorithm(start int, edges [][][]int) []int {
 	return buf
 }
 
-func bfs(dst []int, s, v int, e [][][]int) []int {
+func bfs(dst []int, s, val int, e [][][]int, v []bool) []int {
 	ce := e[s]
 	for i := 0; i < len(ce); i++ {
 		c := ce[i]
-		dst[c[0]] = min(dst[c[0]], v+c[1])
+		dst[c[0]] = min(dst[c[0]], val+c[1])
 	}
 	for i := 0; i < len(ce); i++ {
 		c := ce[i]
-		dst = bfs(dst, c[0], dst[c[0]], e)
+		if v[c[0]] {
+			continue
+		}
+		v[c[0]] = true
+		dst = bfs(dst, c[0], dst[c[0]], e, v)
 	}
 	return dst
 }
--- dijkstras-algorithm/dijkstras_algorithm_test.go
+++ dijkstras-algorithm/dijkstras_algorithm_test.go
@@ -7,16 +7,34 @@ import (
 )
 
 func TestDijkstrasAlgorithm(t *testing.T) {
-	start := 0
-	edges := [][][]int{
-		{{1, 7}},
-		{{2, 6}, {3, 20}, {4, 3}},
-		{{3, 14}},
-		{{4, 2}},
-		{},
-		{},
-	}
-	expected := []int{0, 7, 13, 27, 10, -1}
-	actual := DijkstrasAlgorithm(start, edges)
-	require.Equal(t, expected, actual)
+	t.Run("0", func(t *testing.T) {
+		start := 0
+		edges := [][][]int{
+			{{1, 7}},
+			{{2, 6}, {3, 20}, {4, 3}},
+			{{3, 14}},
+			{{4, 2}},
+			{},
+			{},
+		}
+		expected := []int{0, 7, 13, 27, 10, -1}
+		actual := DijkstrasAlgorithm(start, edges)
+		require.Equal(t, expected, actual)
+	})
+	t.Run("1", func(t *testing.T) {
+		start := 0
+		edges := [][][]int{
+			{{1, 1}, {3, 1}},
+			{{2, 1}},
+			{{6, 1}},
+			{{1, 3}, {2, 4}, {4, 2}, {5, 3}, {6, 5}},
+			{{5, 1}},
+			{{4, 1}},
+			{{5, 2}},
+			{{0, 7}},
+		}
+		expected := []int{0, 7, 13, 27, 10, -1}
+		actual := DijkstrasAlgorithm(start, edges)
+		require.Equal(t, expected, actual)
+	})
 }
